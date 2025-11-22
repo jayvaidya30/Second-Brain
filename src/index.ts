8397897583cff989f4913df9a41494c46054e677
Jay <jayvaidya3009@gmail.com>
2025-11-22 14:27:33 +0530

    Add get and delete content route

--- src/index.ts
+++ src/index.ts
@@ -83,7 +83,23 @@ app.post("/api/v1/content", userMiddleware, async (req, res) => {
   })
 });
 
-app.delete("/api/v1/content", (req, res) => {});
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+  //@ts-ignore
+  const userId = req.userId;
+
+  const content = await ContenModel.find({
+    userId: userId
+  }).populate("userId", "username")
+
+  res.json({
+    content
+  })
+});
+
+app.get("/api/v1/content", async (req , res))
+
+
+
 
 app.get("/api/v1/brain/:shareLink", (req, res) => {});
 
